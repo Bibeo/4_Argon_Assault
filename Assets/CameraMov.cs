c60dde19b1393373fd19ccde168fc401c5b7a910
Pierre Le Bever <pierrelebever@gmail.com>
2020-04-10 14:29:06 +0200

    Game + Terrain + Player (movements + Particles) + Obstacles + Waipont + Circuit

---
+++ Assets/CameraMov.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Camera.main.fieldOfView = (Camera.main.fieldOfView + 0.5f);
+    }
+}
---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    //TO not destroy the gameobject and so the music on it when we switch level
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Invoke("LoadFirstLevel", 3f);
+    }
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
