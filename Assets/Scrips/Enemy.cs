fb42966f160bb43224a9c2ad06363493362d64b0
Pierre Le Bever <pierrelebever@gmail.com>
2020-04-21 16:46:17 +0200

    Enemy health system

--- Assets/Scrips/Enemy.cs
+++ Assets/Scrips/Enemy.cs
@@ -12,8 +12,10 @@ public class Enemy : MonoBehaviour
     //[SerializeField] int scoreHit = 12; In the tutorial
     //My own bonus points :
     [SerializeField] int scoreHitBase = 12;
-    [SerializeField] int scorePerHit;
+    int scorePerHit;
     [SerializeField] int bonusTimeScore = 42;
+
+    [SerializeField] int Hits = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -30,12 +32,27 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        if (Hits <= 0)
+        {
+            killEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
         scorePerHit = (bonusTimeScore - Mathf.RoundToInt(Time.time)) * scoreHitBase;
         scoreBoard.ScoreHit(scorePerHit);
+        //Shorter way to say : maxHits = maxHits - 1;
+        Hits--;
+        // todo consider hit FX
+    }
+
+    private void killEnemy()
+    {
         //We call first, the gameobject to instantiate, then we set its position, finally its rotation (if needed), here we're just reading the rotation not acting
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
-        
     }
 }
